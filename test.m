d723ffa5bcc2a6f0700911032fa360cae53c6c28
lpx1233 <956805832@qq.com>
2017-07-19 16:18:46 +0800

    Modified test.m.

--- test.m
+++ test.m
@@ -5,6 +5,9 @@ clc;
 %% change the current directory to the folder which contains this m file
 cd(fileparts(which(mfilename)));
 
+% TODO: Remove tic/toc
+tic
+
 RawSignal = importdata('.\Sampled Data\RoF\wireless\rof_btb_eml20170713.dat');
 
 SampleRate = 600e9;
@@ -12,50 +15,55 @@ OSCRate = 120e9;
 DataRate = 12.5e9;
 OverSamplingRatio = SampleRate / DataRate;
 
-SampledSignal = 100 * (RawSignal(:, 2) - mean(RawSignal(:, 2)));
+SampledSignal = (RawSignal(:, 2) - mean(RawSignal(:, 2))) / std(RawSignal(:, 2));
 SampledSignal = resample(SampledSignal, SampleRate, OSCRate);
 
 t = RawSignal(:, 1);
-t = interp1(1:length(t), t, 1:OSCRate/SampleRate:length(t)+1);
+t = interp1(1:length(t), t, 1 : OSCRate/SampleRate : length(t)+1);
 t = t';
 t = t(1:end-1);
 
-index = find(abs(t) < 1e-9);
-
-fc = 25.5e9;
+fc = 25e9;
 lo = cos(2*pi*fc*t) + i * -sin(2*pi*fc*t);
 % plot(t(index), SampledSignal(index), t(index), real(lo(index)), 'r')
 
 r = lowPassFilter25G(SampledSignal .* lo);
-
-figure;
-subplot(2,1,1);
-plot(t(index), real(r(index)))
-subplot(2,1,2);
-plot(t(index), imag(r(index)))
+% figure;
+% subplot(2,1,1);
+% plot(t(index), real(r(index)))
+% subplot(2,1,2);
+% plot(t(index), imag(r(index)))
 
 MatchFilter = ones(SampleRate/DataRate, 1);
 r = conv(r, MatchFilter, 'same');
-
-figure;
-subplot(2,1,1);
-plot(t(index), real(r(index)))
-subplot(2,1,2);
-plot(t(index), imag(r(index)))
-
+% figure;
+% subplot(2,1,1);
+% plot(t(index), real(r(index)))
+% subplot(2,1,2);
+% plot(t(index), imag(r(index)))
 scatterplot(r)
 
-carsync = comm.CarrierSynchronizer('Modulation', 'PAM', 'SamplesPerSymbol', OverSamplingRatio);
-symsync = comm.SymbolSynchronizer(...
-    'TimingErrorDetector', 'Early-Late (non-data-aided)', ...
-    'SamplesPerSymbol', OverSamplingRatio);
-
+carsync = comm.CarrierSynchronizer('Modulation', 'PAM', ...
+  'SamplesPerSymbol', OverSamplingRatio, ...
+  'ModulationPhaseOffset', 'Custom', ...
+  'CustomPhaseOffset', 0);
 r = step(carsync, r);
-
+r = r(find(isnan(r) == 0));
 scatterplot(r)
 
+r = -real(r);
+r = (r - mean(r)) / std(r);
+% symsync = comm.SymbolSynchronizer(...
+%   'TimingErrorDetector', 'Early-Late (non-data-aided)', ...
+%   'SamplesPerSymbol', OverSamplingRatio);
+% tic
 % r = step(symsync, r);
+% toc
 % step(cd3, r)
+
+% TODO: Remove tic/toc
+toc
+tic
 
 OriginalSignal = importdata('.\Original Data\Original_Data.txt');
 OriginalData_port1 = OriginalSignal;
@@ -65,13 +73,15 @@ OriginalData_port1 = OriginalSignal;
 shiftnum = 58;
 OriginalData_port2 = [~(OriginalSignal(end - shiftnum + 1 : end));
 											~(OriginalSignal(1 : end - shiftnum))];
-OriginalData = OriginalData_port1 + 2 * OriginalData_port2;
-% OriginalData = OriginalData_port1;
-SampledSignal = -real(r);
-CorrelationResult = zeros(length(SampledSignal) - OverSamplingRatio * length(OriginalData) + 1, 1);
+% OriginalData = OriginalData_port1 + 2 * OriginalData_port2;
+OriginalData = OriginalData_port1;
+CorrelationResult = zeros(length(r) - OverSamplingRatio * length(OriginalData) + 1, 1);
 % Need 40min to extract signal
 parfor i = 1 : length(CorrelationResult)
-  fprintf('Generating %dth result\n', i);
-  CorrelationResult(i) = sum(SampledSignal(i : OverSamplingRatio : i + OverSamplingRatio * length(OriginalData) - 1) .* OriginalData);
+  % fprintf('Generating %dth result\n', i);
+  CorrelationResult(i) = sum(r(i : OverSamplingRatio : i + OverSamplingRatio * length(OriginalData) - 1) .* OriginalData);
 end
 plot(CorrelationResult)
+
+% TODO: Remove tic/toc
+toc
